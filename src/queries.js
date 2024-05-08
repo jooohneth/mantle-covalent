9e179ab98ee748da5d402e7eb37c02af2cf2256d
John <jooohn.eth@gmail.com>
2024-05-07 20:28:22 -0400

    query token balances

--- src/queries.js
+++ src/queries.js
@@ -2,3 +2,21 @@ import "dotenv/config";
 import { CovalentClient } from "@covalenthq/client-sdk";
 
 const client = new CovalentClient(process.env.API_KEY);
+
+// BALANCES
+const fetchBalances = async () => {
+  const response = await client.BalanceService.getTokenBalancesForWalletAddress(
+    "mantle-mainnet",
+    "0x40FEfD52714f298b9EaD6760753aAA720438D4bB",
+    {
+      nft: true,
+      quoteCurrency: "USD",
+      noNftFetch: true,
+      noSpam: true,
+      noNftAssetMetada2a: false,
+    }
+  );
+  console.log(response.data);
+};
+
+fetchBalances();
