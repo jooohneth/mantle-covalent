50fbf9e780c6601a6cc1d74fd2f42344c32eb939
Thai Nguyen <jooohn.eth@gmail.com>
2024-05-15 19:01:43 -0400

    setup: import, goldrushprovider, api-key, params

--- goldrush-demo/src/App.jsx
+++ goldrush-demo/src/App.jsx
@@ -1,7 +1,22 @@
+import {
+  GoldRushProvider,
+  NFTWalletTokenListView,
+  TokenBalancesListView,
+  TokenTransfersListView,
+  AddressActivityListView,
+} from "@covalenthq/goldrush-kit";
+import "@covalenthq/goldrush-kit/styles.css";
+
 function App() {
+  const params = {
+    networks: ["mantle-mainnet"],
+    address: "0x94FEC56BBEcEaCC71c9e61623ACE9F8e1B1cf473",
+    token: "0xcDA86A272531e8640cD7F1a92c01839911B90bb0",
+  };
+
   return (
     <div className="App">
-      <div></div>
+      <GoldRushProvider apikey={process.env.API_KEY}></GoldRushProvider>
     </div>
   );
 }
